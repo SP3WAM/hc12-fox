c0ff11c7e9c7a1ed667dde74976c0e970d1d76ad
Witold Markowski <witold.a.markowski@gmail.com>
2026-02-17 20:22:27 +0000

    Add Si4438 RF config to cover relays inputs starting from 431.050 MHz

---
+++ lib/core/radio_config_Si4438_431_000_channels.h
@@ -0,0 +1,6 @@
+
+// The channel 0 has the freq 431.000 MHz. 
+// Channel step is there defined as 12.5 kHz, so this gives us the following channels:
+
+#define CHANNEL_431_000  0 // 431.000 MHz 
+#define CHANNEL_431_600 48 // 431.600 MHz as a relay input
